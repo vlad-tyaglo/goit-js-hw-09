46a4960edf30c22ca5a1661f95a73325157ca459
vlad-tyaglo <113830540+vlad-tyaglo@users.noreply.github.com>
2023-01-24 19:10:52 +0000

    Deploying to gh-pages from @ vlad-tyaglo/goit-js-hw-09@40514900f6de1df79db37155a55b7176ce3f93cd 🚀

---
+++ 01-color-switcher.04880b2c.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){t.style.backgroundColor=r(),o=setInterval((function(){t.style.backgroundColor=r()}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,n.disabled=!0}));var o=null;function r(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}}();
+//# sourceMappingURL=01-color-switcher.04880b2c.js.map
